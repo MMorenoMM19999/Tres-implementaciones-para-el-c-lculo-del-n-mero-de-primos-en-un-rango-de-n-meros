99fae40e37c75bab471309e9bd812a1d4fbf1928
Moreno <106407303+MMorenoMM19999@users.noreply.github.com>
2024-05-29 14:47:58 -0500

    Add files via upload

---
+++ TareaOS/Estrategia2.c
@@ -0,0 +1,55 @@
+/**Estrategia 2: Usar un vector global para almacenar el número de primos encontrados por cada hilo*/
+/**Miguel Angel Moreno Romero (2125737)
+Juan Esteban Guerrero Camacho (2040798)
+*/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 4
+int primesFound[NUM_THREADS];
+
+typedef struct {
+    int start;
+    int end;
+    int index;
+} Range;
+
+void *countPrimes(void *arg) {
+    Range *range = (Range *)arg;
+    int count = 0;
+    for (int num = range->start; num <= range->end; num++) {
+        int prime = 1;
+        if (num < 2) prime = 0;
+        for (int i = 2; i*i <= num; i++) {
+            if (num % i == 0) {
+                prime = 0;
+                break;
+            }
+        }
+        if (prime) count++;
+    }
+    primesFound[range->index] = count;
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+    Range ranges[NUM_THREADS];
+    int totalPrimes = 0;
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        ranges[i].start = i * 25 + 1;
+        ranges[i].end = (i + 1) * 25;
+        ranges[i].index = i;
+        pthread_create(&threads[i], NULL, countPrimes, &ranges[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+        totalPrimes += primesFound[i];
+    }
+
+    printf("Total primes: %d\n", totalPrimes);
+    return 0;
+}
